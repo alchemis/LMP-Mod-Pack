16b954c8a392847fc97a7ac15e0c386d245c8943
Tilation <gasparcorrarello@gmail.com>
2022-05-13 18:56:02 -0300

    Added signing, erased useless files

--- ModLoaderSource/ModLoader/Properties/AssemblyInfo.cs
+++ ModLoaderSource/ModLoader/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModLoader")]
+[assembly: AssemblyTitle("TMP - ModLoader")]
 [assembly: AssemblyDescription("Pokemon Reborn ModLoader")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
